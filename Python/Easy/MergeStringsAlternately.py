d9b46c55e0476218ce315c5c40f91c5b9cc817a1
Joatan <joatansampaio@live.com>
2023-09-25 14:21:04 -0500

    update

---
+++ Python/Easy/MergeStringsAlternately.py
@@ -0,0 +1,30 @@
+
+'''
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+'''
+
+def mergeAlternately(word1, word2): 
+    """
+    :type word1: str
+    :type word2: str
+    :rtype: str
+    """
+    result = ""
+    length = min(len(word1), len(word2))
+
+    for i in range(length):
+        result += word1[i] + word2[i]
+
+    if len(word1) > len(word2):
+        result += word1[length:]
+        return result
+    result += word2[length:]
+    return result
+
+print(mergeAlternately("abc", "pqr"))
